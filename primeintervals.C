d8154f0d88d7777f58784ab86318b605d3b9b3b0
sandhiyakarthicsaran <35221190+sandhiyakarthicsaran@users.noreply.github.com>
2018-02-07 16:53:36 +0530

    prime in intervals

---
+++ primeintervals.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int n,i,j,min,max,flag=0;
+printf("Enter the two intervals:\n");
+scanf("%d%d",&min,&max);
+for(i=min+1;i<max;i++)
+{
+flag=0;
+for(j=2;j<=sqrt(i);j++)
+{
+if(i%j==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("%d",i);
+}
+}
+return 0;
+}
